86a00fac82e427da040c084b32a21be255df3521
Azel <azelkarthak99@gmail.com>
2025-07-11 20:43:52 +0530

    Update URLs in CardSections.js to point to the production server

--- frontend/src/pages/Solutions/CardSections.js
+++ frontend/src/pages/Solutions/CardSections.js
@@ -40,7 +40,7 @@ const solutions = [
     icon: <RobotOutlined style={{ fontSize: "30px", color: "#1890ff" }} />,
     bgImages: genai,
     action: () => {
-      window.open("http://127.0.0.1:5001", "_blank");
+      window.open("http://54.84.135.175:5001", "_blank");
     }
   },
   {
@@ -49,7 +49,7 @@ const solutions = [
     icon: <BugOutlined style={{ fontSize: "30px", color: "#52c41a" }} />,
     bgImages: defectpattern,
     action: () => {
-      window.open("http://127.0.0.1:5002/defect", "_blank");
+      window.open("http://54.84.135.175:5002/defect", "_blank");
     }
   },
   {
@@ -58,7 +58,7 @@ const solutions = [
     icon: <ExperimentOutlined style={{ fontSize: "30px", color: "#faad14" }} />,
     bgImages: requirements,
     action: () => {
-      window.open("http://127.0.0.1:5002/jira", "_blank");
+      window.open("http://54.84.135.175:5002/jira", "_blank");
     }
   },
   {
@@ -67,7 +67,7 @@ const solutions = [
     icon: <ThunderboltOutlined style={{ fontSize: "30px", color: "#eb2f96" }} />,
     bgImages: intelligence,
     action: () => {
-      window.open("http://127.0.0.1:5000", "_blank");
+      window.open("http://54.84.135.175:5000", "_blank");
     }
   },
   {
@@ -76,7 +76,7 @@ const solutions = [
     icon: <DashboardOutlined style={{ fontSize: "30px", color: "#722ed1" }} />,
     bgImages: performance,
     action: () => {
-      window.open("http://127.0.0.1:5002/performance", "_blank");
+      window.open("http://54.84.135.175:5002/performance", "_blank");
     }
   },
   {
@@ -85,7 +85,7 @@ const solutions = [
     icon: <TeamOutlined style={{ fontSize: "30px", color: "#13c2c2" }} />,
     bgImages: people,
     action: () => {
-      window.open("http://127.0.0.1:5002/test", "_blank");
+      window.open("http://54.84.135.175:5002/test", "_blank");
       // Add your navigation logic here for this card
     }
   },
